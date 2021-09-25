91a7ab0711bd5dce58e3c82e1ffa48b62c1d4ec4
ktht <karlehataht@gmail.com>
2021-09-25 19:23:43 +0300

    Executable rights to eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from data import COCODetection, get_label_map, MEANS, COLORS
 from yolact import Yolact
 from utils.augmentations import BaseTransform, FastBaseTransform, Resize
